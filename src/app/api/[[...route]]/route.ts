9c089abe6afcef22cd40b713f6b3aeff3253d7e5
BobbyLin23 <linzhangsheng23@gmail.com>
2025-02-21 20:21:35 +0800

    feat: add base URL logging and session handling in auth middleware

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -12,15 +12,16 @@ const app = new Hono<{
 
 app.use('*', async (c, next) => {
   const session = await auth.api.getSession({ headers: c.req.raw.headers })
-
+  console.log('auth baseURL', auth.options.baseURL)
+  console.log('middleware session', session)
   if (!session) {
     c.set('user', null)
     c.set('session', null)
     return next()
   }
 
-  c.set('user', session.user)
   c.set('session', session.session)
+  c.set('user', session.user)
   return next()
 })
 
